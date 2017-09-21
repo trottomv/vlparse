cffc6954f79810eb16ba29b0443e2bab6630e8bc
Matteo Vitali <trottomv@gmail.com>
2017-09-21 17:56:23 +0200

    add gemfile

--- parse.rb
+++ parse.rb
@@ -2,7 +2,7 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-
+require 'date'
 
 def url
   url = "http://web.legabasket.it"
@@ -20,23 +20,30 @@ def gameurl
   # end
 end
 
-gameurl.each do |gameurl|
-  puts gameurl
-end
+# gameurl.each do |gameurl|
+#   puts "#{url}#{gameurl}"
+# end
 
 def gamedatetime
   datetime = page.xpath('//a[contains(text(), ":")]')
 end
 
-gamedatetime.each do |gamedatetime|
-  puts gamedatetime.text
+# gamedatetime.each do | gamedatetime |
+#   puts gamedatetime.text
+# end
+
+gamedatetime.each do | d |
+  d = gamedatetime.text
+  if d < "#{Time.now.strftime('%d/%m/%Y%H:%M')}"
+    puts d
+  end
 end
 
 
 def gameresult
-  gameurl.each do |gameurl|
-    gamestand = Nokogiri::HTML(open("#{url}#{gameurl}"))
-    gamestand.css(".game-result-container,.page-title").each do |gamestand|
+  gameurl.each do | gameurl |
+    r = Nokogiri::HTML(open("#{url}#{gameurl}"))
+    r.css(".game-result-container,.page-title").each do |gamestand|
         puts gamestand.content
     end
   end
