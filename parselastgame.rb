8250be007ec924404c3f023678792282f42ce3b7
Matteo Vitali <trottomv@gmail.com>
2017-09-22 23:08:20 +0200

    small correction

--- parselastgame.rb
+++ parselastgame.rb
@@ -7,7 +7,8 @@ def url
 end
 
 def teampage
-  team = "http://web.legabasket.it/team/1296/consultinvest_pesaro"
+  team = "http://web.legabasket.it/team/1296/consultinvest_pesaro" #2016
+  # team = "http://web.legabasket.it/team/1314/vl_pesaro" #2017
   Nokogiri::HTML(open("#{team}"))
 end
 
@@ -52,9 +53,16 @@ end
 
 # def condition
 #   Date.parse('dategame.text') < Date.today.strftime('%d/%m/%Y%H:%M') #"07/05/201720:45"
+
+  # if (01/01/2016..01/01/2017) === dategame
+  #   puts true
+  # else
+  #   puts false
+  # end
+
 # end
 
-puts "#{dategame.text.split(', ')[0].gsub(" - ", "").lstrip[0..-6]} #{round.text.lstrip.chop}"
+puts "#{dategame.text.split(', ')[0].gsub(" - ", "").lstrip[0..-6]} #{round.text.strip}"
 # puts stadium.text.split(', ')[1].split(': ')[1].gsub(" - ", "").lstrip.chop
-puts "#{team1.text} VS #{team2.text} #{gameresult.text.lstrip.chop} "
+puts "#{team1.text} VS #{team2.text} #{gameresult.text.strip} "
 puts "Tabellino: #{url}/game/#{lastgameurl.first.split('/')[2]}/"
