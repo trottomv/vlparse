98ca6b053e2ec5ca95a534a5fe3bf8c6149477b4
Matteo Vitali <trottomv@gmail.com>
2017-09-22 10:48:35 +0200

    better methods defiinition

--- parselastgame.rb
+++ parselastgame.rb
@@ -6,7 +6,7 @@ def url
   url = "http://web.legabasket.it"
 end
 
-def page
+def teampage
   team = "http://web.legabasket.it/team/1296/consultinvest_pesaro"
   page = Nokogiri::HTML(open("#{team}"))
 end
@@ -14,84 +14,49 @@ end
 def lastgameurl
   # gameurl = page.xpath('//a[contains(text(""), "matchResult")]').map { |link| link['href'] }.each do | gameurl |
   # gameurl = page.css(".other-team-information").each do | gameurl |
-  lastgameurl = page.css("a.matchResult").map { |link| link['href'] }
+  lastgameurl = teampage.css("a.matchResult").map { |link| link['href'] }
 end
 
-# puts lastgameurl.first.split('/')[2]
-
-def gameurl
+def parse
   gameurl = "#{url}/game/#{lastgameurl.first.split('/')[2]}/"
+  parse = Nokogiri::HTML(open("#{gameurl}"))
 end
 
-def lastgamestand
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".game-result-container").each do | gameresult |
-    puts gameresult.text
-  end
+def gamestand
+  parse.css(".game-result-container")
 end
-
-# puts lastgamestand
 
 def dategame
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".page-title").each do | title |
-    puts title.text.split(', ')[0].gsub(" - ", "").to_str
-  end
+  parse.css(".page-title")
 end
-
-# puts dategame
 
 def stadium
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".page-title").each do | title |
-    puts title.text.split(', ')[1].split(': ')[1].gsub(" - ", "").to_str
-  end
+  parse.css(".page-title")
 end
 
-# puts stadium
 
 def round
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".titleBig").each do | title |
-    puts title.text
-  end
+  parse
+  parse.css(".titleBig")
 end
-
-# puts round
-
 
 def team1
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".team-name1").each do | title |
-    puts title.text
-  end
+  parse.css(".team-name1")
 end
-
-# puts team1
-
 
 def team2
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".team-name2").each do | title |
-    puts title.text
-  end
+  parse.css(".team-name2")
 end
-
-# puts team2
 
 def gameresult
-  page = Nokogiri::HTML(open("#{gameurl}"))
-  lastgamestand = page.css(".game-total-result").each do | result |
-    puts result.text
-  end
+  parse.css(".game-total-result")
 end
-
-# puts gameresult
 
 def condition
   Date.parse('dategame.text') < Date.today.strftime('%d/%m/%Y%H:%M') #"07/05/201720:45"
 end
 
-# puts condition
-
-puts "#{round}#{dategame}#{stadium}#{team1}#{gameresult}#{team2} Tabellino: #{url}/game/#{lastgameurl.first.split('/')[2]}/"
+puts round.text.lstrip.chop
+puts "#{team1.text} VS #{team2.text}"
+puts gameresult.text.lstrip.chop
+puts "Tabellino: #{url}/game/#{lastgameurl.first.split('/')[2]}/"
