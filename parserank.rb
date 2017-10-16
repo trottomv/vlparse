f85a24b3cf4d350d9ecd50932a6e31cffe621212
Matteo Vitali <trottomv@gmail.com>
2017-10-16 21:37:10 +0200

    clean code

--- parserank.rb
+++ parserank.rb
@@ -16,44 +16,20 @@ class Rank
   def rankparse
     parse.css('table').search('tr')[3..25].map.each do | cell |
     {
-      position: cell.search('th, td')[0..1].text,
+      position: cell.search('th, td')[0].text,
+      team: cell.search('th, td')[1].text,
       point: cell.search('th, td')[2].text
     } #.each do | rankteam |
     end
   end
 
-  def rankteam
-    parse.css('table').search('tr')[3..25].map { | cell |
-      cell.search('th, td')[1].text.strip
-    } #.each do | rankteam |
-    # end
-  end
-
-  def rankpoint
-    parse.css('table').search('tr')[3..25].map { | cell |
-      cell.search('th, td')[2].text.strip
-    } #.each do | rankpoint |
-    # end
-  end
-
   def message
-      rankparse.each_with_index do | rank, index |
-          # message = rank[:position]
-      # rank.map do |message|
-          message = "#{rank[:position]} #{rank[:point]}"
-          message
-          # return message
-          # rank[:point]
-          # return message
-        # end
+      rankparse.map do | rank |
+        message = " #{rank[:position].chomp('.')} [#{rank[:point]}pt] #{rank[:team]}"
+        message
       end
   end
 
 end
 
-# puts "#{Rank.new.rankposition} #{Rank.new.rankteam} #{Rank.new.rankpoint}"
 puts Rank.new.message
-
-# Rank.new.rankposition.map do | rankposition |
-#   puts "#{Rank.new.rankposition[3..25]} #{Rank.new.rankteam[3..25]} #{Rank.new.rankpoint[3..25]}"
-# end
